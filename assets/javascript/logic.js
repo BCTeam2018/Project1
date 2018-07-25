22930ca890e9e73d85967e62533868e40bbc2ae8
JPDearing <justin.p.dearing@gmail.com>
2018-07-24 19:06:30 -0500

    Eventful API
    Working Eventful API with some extra nonsense. Still working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,57 @@
-//Initial branch commit for Eventbrite API
+
+$("button").on("click", function() {
+    //var userEvent = $(this).attr("what");
+    //var userLocation = $(this).attr("where");
+    var proxy = "https://cors-anywhere.herokuapp.com/";
+    var queryURL = "http://austin.eventful.com/json/events?&l=Austin&app_key=xDx7HLFpRJgTBLJL";
+   var queryURLAustin = "http://api.eventful.com/json/events/search?location=Austin&date=Future&app_key=xDx7HLFpRJgTBLJL"
+     $.ajax({
+    url: proxy + queryURLAustin,
+    method: "GET"
+    })
+  .done(function(response) {
+    // console.log(response["total_items"]);
+    // console.log(JSON.stringify(response));
+    let newResponse = JSON.parse(response);
+    console.log(newResponse.events.event[0].title);
+    console.log(newResponse["total_items"]);
+    console.log(newResponse)
+   // console.log(response)
+    });
+  })
+  
+  
+  
+  
+  
+  //"http://api.eventful.com/json/events/search?keywords=concerts&location=Austin=Future&app_key=xDx7HLFpRJgTBLJL";
+  
+  ///Plug in the Google Maps API here __________________________
+  
+  //  $("button").on("click", function() {
+  //  var city = $(this).attr("data-name");
+  
+  //  var queryURL =
+  //  })
+  
+  //End Google Maps API call here__________________________
+  
+  
+  
+  // TEST ENVIRONMENT
+  
+  //User input button event
+  
+  //$("#submit-btn").on("click", function(event) {
+  //  event.preventDefault();
+  //  var userCity = $("#city-input").val().trim();
+  //  var userKeyword = $("#keyword-input").val().trim();
+  //  eventfulArray.push(userCity.name, userKeyword);
+  //});
+  
+  //var eventfulArray = [];
+  //console.log(eventfulArray);
+  
+  
+  
+  
